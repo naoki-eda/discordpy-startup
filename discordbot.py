36ae3217ca19cb30cb091f026748822e892f22ed
naoki-eda <74550815+naoki-eda@users.noreply.github.com>
2020-11-17 00:57:46 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -3,8 +3,8 @@ import discord
 from discord.ext import tasks
 from datetime import datetime 
 
-TOKEN = "Nzc3ODg4MTEzNzg2NjE3OTA2.X7J-wg.-wL1XP8s3eVPU0PWv3-ZebUELus" #トークン
-CHANNEL_ID = 687202041856000003 #チャンネルID
+TOKEN = "" #トークン
+CHANNEL_ID = #チャンネルID
 # 接続に必要なオブジェクトを生成
 client = discord.Client()
 
