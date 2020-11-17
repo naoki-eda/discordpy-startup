4d0c3ed49ec5eac03894d9363b1d630501105b7f
naoki-eda <74550815+naoki-eda@users.noreply.github.com>
2020-11-17 23:17:36 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,30 +1,10 @@
-#coding:UTF-8
-import os,discord
-from discord.ext import tasks
-from datetime import datetime 
+from discord.ext import commands
+import os
+import traceback
 
 bot = commands.Bot(command_prefix='/')
-token = os.environ["DISCORD_BOT_TOKEN"] #トークン
-channel_id = os.environ["CHANNEL_ID"] #チャンネルID
-# 接続に必要なオブジェクトを生成
-client = discord.Client()
+token = os.environ['DISCORD_BOT_TOKEN']
 
-# 60秒に一回ループ
-@tasks.loop(seconds=60)
-async def loop():
-    # 現在の時刻
-    now = datetime.now().strftime('%H:%M')
-    if now == '11:15':
-        channel = client.get_channel(channel_id)
-        await channel.send('てすと')  
-
-#ループ処理実行
-loop.start()
-# Botの起動とDiscordサーバーへの接続
-client.run(token)
-
-#イベント処理
-bot = commands.Bot(command_prefix='/')
 
 @bot.event
 async def on_command_error(ctx, error):
@@ -32,8 +12,10 @@ async def on_command_error(ctx, error):
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
+
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
+
 
 bot.run(token)
