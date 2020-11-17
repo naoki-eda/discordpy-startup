98056a55b31fb9aa5e98e6ab9f3cb0db7953d4cf
naoki-eda <74550815+naoki-eda@users.noreply.github.com>
2020-11-17 23:25:18 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,21 +1,23 @@
-from discord.ext import commands
-import os
-import traceback
+#coding:UTF-8
+import os,discord
+from discord.ext import tasks
+from datetime import datetime 
 
-bot = commands.Bot(command_prefix='/')
-token = os.environ['DISCORD_BOT_TOKEN']
+token = os.environ['DISCORD_BOT_TOKEN'] #トークン
+channel_id = os.environ['CHANNEL_ID'] #チャンネルID
+# 接続に必要なオブジェクトを生成
+client = discord.Client()
 
+# 60秒に一回ループ
+@tasks.loop(seconds=60)
+async def loop():
+    # 現在の時刻
+    now = datetime.now().strftime('%H:%M')
+    if now == '23:30':
+        channel = client.get_channel(channel_id)
+        await channel.send('てすと')  
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
-
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-
-
-bot.run(token)
+#ループ処理実行
+loop.start()
+# Botの起動とDiscordサーバーへの接続
+client.run(token)
