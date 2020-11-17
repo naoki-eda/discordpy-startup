d02b32592468496e59928a9c22e302cccb0b1254
naoki-eda <74550815+naoki-eda@users.noreply.github.com>
2020-11-17 23:08:40 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -3,8 +3,9 @@ import os,discord
 from discord.ext import tasks
 from datetime import datetime 
 
-TOKEN = os.environ["DISCORD_BOT_TOKEN"] #トークン
-CHANNEL_ID = os.environ["CHANNEL_ID"] #チャンネルID
+bot = commands.Bot(command_prefix='/')
+token = os.environ["DISCORD_BOT_TOKEN"] #トークン
+channel_id = os.environ["CHANNEL_ID"] #チャンネルID
 # 接続に必要なオブジェクトを生成
 client = discord.Client()
 
@@ -13,15 +14,26 @@ client = discord.Client()
 async def loop():
     # 現在の時刻
     now = datetime.now().strftime('%H:%M')
-    if now == '01:33':
-        channel = client.get_channel(CHANNEL_ID)
+    if now == '11:15':
+        channel = client.get_channel(channel_id)
         await channel.send('てすと')  
 
 #ループ処理実行
 loop.start()
 # Botの起動とDiscordサーバーへの接続
-client.run(TOKEN)
+client.run(token)
+
+#イベント処理
+bot = commands.Bot(command_prefix='/')
+
+@bot.event
+async def on_command_error(ctx, error):
+    orig_error = getattr(error, "original", error)
+    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
+    await ctx.send(error_msg)
 
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
+
+bot.run(token)
