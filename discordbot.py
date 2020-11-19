ea45b8cbfe0fa9fe6bdd4b5eae35823df230f56c
naoki-eda <74550815+naoki-eda@users.noreply.github.com>
2020-11-19 22:25:03 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -9,38 +9,19 @@ channel_id = os.environ['CHANNEL_ID'] #チャンネルID
 # 接続に必要なオブジェクトを生成
 client = discord.Client()
 
-# @tasks.loop(seconds=60)
-# async def loop():
-#     print(datetime.now().strftime("%Y/%m/%d %H:%M:%S"), "start")
-#     print("token", token)
-#     print("channel_id", channel_id)
-# #     await client.wait_until_ready()
-#     print(client.is_ready())
-#     channel = client.get_channel(channel_id)
-#     print(channel)
-#     await channel.send('てすと')  
-
-# @client.event
-# async def on_ready():
-#     channel = client.get_channel(channel_id)
-#     print(channel)
-#     await channel.send('てすと')  
-#     #ループ処理実行
-#     loop.start()
-
-# # Botの起動とDiscordサーバーへの接続
-# client.run(token)
-
-channel_sent = None
-@tasks.loop(seconds=10)
-async def send_message_every_10sec():
-    await channel_sent.send("10秒経ったよ")
+@tasks.loop(seconds=60)
+async def loop():
+    print(datetime.now().strftime("%Y/%m/%d %H:%M:%S"), "start")
+    print(client.is_ready())
+    channel = client.get_channel(channel_id)
+    if channel != None :
+        print(channel)
+        await channel.send('てすと')  
 
 @client.event
 async def on_ready():
-    global channel_sent 
-    channel_sent = client.get_channel(channel_id)
-    send_message_every_10sec.start() #定期実行するメソッドの後ろに.start()をつける
+    #ループ処理実行
+    loop.start()
 
-
+# Botの起動とDiscordサーバーへの接続
 client.run(token)
